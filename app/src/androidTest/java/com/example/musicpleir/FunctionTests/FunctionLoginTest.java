293b2651adeb67f99051cc262ca32e024dc8288a
Pathetic384 <papcusun2125@gmail.com>
2024-05-22 16:10:57 +0700

    reg + log tests

--- app/src/androidTest/java/com/example/musicpleir/FunctionTests/FunctionLoginTest.java
+++ app/src/androidTest/java/com/example/musicpleir/FunctionTests/FunctionLoginTest.java
@@ -9,6 +9,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -31,12 +32,16 @@ import com.example.musicpleir.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FunctionLoginTest {
 
     @Rule
@@ -167,6 +172,49 @@ public class FunctionLoginTest {
                                 withParent(withId(android.R.id.content)))),
                         isDisplayed()));
         textView.check(matches(withText("Please enter your Email")));
+    }
+
+    @Test
+    public void z_loginSuccessTest() {
+        ViewInteraction textInputEditText = onView(
+                allOf(withId(R.id.email),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText("tester@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText2.perform(replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.login_btn), withText("Login"),
+                        childAtPosition(
+                                allOf(withId(R.id.main),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                6),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        onView(isRoot()).perform(waitFor(5000));
+
+        ViewInteraction textView = onView(
+                allOf(withText("SONGS"),
+                        withParent(allOf(withContentDescription("Songs"),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("SONGS")));
     }
 
     private static Matcher<View> childAtPosition(
